9d2fa0de7b0fb7ecb9c4459317a5b44a6ccc9177
JETWAY COMPUTER <JETWAY COMPUTER@DESKTOP-LR12QTM>
2021-08-14 14:34:03 +0530

    addddddddd

---
+++ src/app/practice.java
@@ -0,0 +1,63 @@
+package app;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+		//While loop
+
+		//1 to 10
+		int i=10;
+
+		/* while(i>0)
+		{
+		System.out.println(i);
+		i--;//i=2
+		}
+		}*/
+		int j=20;
+		do
+		{
+		System.out.println(j);
+		j++;
+
+		}while(j>30);// 1 loop of execution is guarantee
+
+
+		//
+
+		// For if loop
+
+		// TODO Auto-generated method stub
+
+		// 1 to 10
+
+		/* for(initization;condition;increment)
+		{
+
+		}*/
+		/* if(5>2)
+		{
+
+
+		System.out.println("success");
+		System.out.println("second step");
+		}
+
+		else
+
+		System.out.println("fail");*/
+
+
+		for(int j1=0;i<10;i=i+3)
+		{
+		if(i==9)
+		System.out.println(" 9 is displayed");
+		else
+		System.out.println("I didnot find");//
+		}
+	}
+
+}
